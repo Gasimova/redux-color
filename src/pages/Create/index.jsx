de8b4688b0d9703591a7a85ef388e40e182d671c
gasimova <ziba@cubics.technology>
2023-12-08 18:12:33 +0400

    ui

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -1,66 +1,91 @@
-import { FormControl, FormHelperText, InputLabel, Input, Grid, CardHeader, CardContent, TextField, CardActions, Button } from '@mui/material'
-import { Field, Form } from 'formik'
-import React from 'react'
+import { Grid, CardHeader } from '@mui/material'
+import { useFormik } from 'formik'
+import React, { useState } from 'react'
+import { SketchPicker, SwatchesPicker } from 'react-color'
+import { DeleteForever } from '@mui/icons-material';
 
-// const useStyle = makeStyles((theme) => ({
-//   padding: {
-//     padding: theme.spacing(3),
-//   },
-//   button: {
-//     margin: theme.spacing(1),
-//   },
-// }))
+let initialValue = {
+  name: '',
+  code: ''
+}
 
 export const Create = () => {
-  // const classes = useStyle()
 
-  const onSubmit = (values) => {
-    console.log(values)
+  const [color, setColor] = useState(initialValue)
+  const [data, setData] = useState([])
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      body: ""
+    },
+    // validate: validateInputs,
+    onSubmit: (values) => {
+      // addPostFetch(values)
+      formik.handleReset()
+    }
+  })
+
+  const handleChangeComplete = (cl) => {
+    setColor({ ...color, code: cl.hex })
   }
+  const handleChangeName = (e) => {
+    setColor({ ...color, name: e.target.value })
+  }
+  const handleFormSubmit = (e) => {
+    if (data.length < 6) {
+      setData((prevData) => [...prevData, color])
+      setColor(initialValue)
+    }else{
+       console.log('data coxdu');
+    }
+  }
+
+  const removeColor = (index) => {
+    setData(data.filter((item, i) => i !== index));
+  };
+
 
   return (
     <>
-      <h1>Create New Color Box</h1>
+      <h1 style={{ textAlign: 'center', marginBottom: 50 }}>Create New Color Box</h1>
 
       <Grid container justify="center" spacing={1}>
         <Grid item md={6}>
           <CardHeader title="REGISTER FORM"></CardHeader>
-          <Form>
-                  <CardContent>
-                    <Grid item container spacing={1} justify="center">
-                      <Grid item xs={12} sm={6} md={6}>
-                        <Field
-                          label="First Name"
-                          variant="outlined"
-                          fullWidth
-                          name="firstName"
-                          // value={values.firstName}
-                          component={TextField}
-                        />
-                      </Grid>
-                      <Grid item xs={12} sm={6} md={6}>
-                        <Field
-                          label="Last Name"
-                          variant="outlined"
-                          fullWidth
-                          name="lastName"
-                          // value={values.lastName}
-                          component={TextField}
-                        />
-                      </Grid>
-                    </Grid>
-                  </CardContent>
-                  <CardActions>
-                    <Button
-                      // disabled={!dirty || !isValid}
-                      variant="contained"
-                      color="primary"
-                      type="Submit"
-                      >
-                      REGISTER
-                    </Button>
-                  </CardActions>
-                </Form>
+          <div className='colorForm'>
+            <label htmlFor='name'>Color name </label>
+            <input onChange={handleChangeName} type='text' value={color.name} name='name' id='name' placeholder='Color name' className='colorInput' />
+            <label htmlFor='code'>Color code </label>
+            <SwatchesPicker onChangeComplete={handleChangeComplete} className='colorPicker' />
+            <input type='text' id='code' name='code' placeholder='Color code' className='colorInput' value={color.code} />
+            <button className='btnForm' type='button'  onClick={handleFormSubmit} >Add Color</button>
+          </div>
+        </Grid>
+        <Grid item md={6}>
+          <CardHeader title="REGISTER FORM"></CardHeader>
+
+          <div className='colorDiv'>
+            {
+              data?.map((item, i) => {
+                return <div className='color' style={{ backgroundColor: item.code, width: 30 }}>
+                  <div className='colorBody'>
+                    <span className='color_name'>{item.name}</span>
+                    <DeleteForever className='delete' onClick={()=>removeColor(i)} />
+                  </div>
+                </div>
+              })
+            }
+            {
+              data.length <= 0 && <h3>Please select colors</h3>
+            }
+          </div>
+
+          <form onSubmit={formik.handleSubmit} className='colorForm'>
+            <label htmlFor='groupName'>Group Name</label>
+            <input type='text' id='groupName' name='groupName' placeholder='Group Name' className='colorInput' />
+            <button className='btnForm' type='submit'>Save</button>
+          </form>
         </Grid>
       </Grid>
     </>
